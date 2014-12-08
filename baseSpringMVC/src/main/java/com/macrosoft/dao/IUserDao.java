8628020dd8f50705bd7c2ad8759988882d4cb4cf
呆呆 <@WangMachine>
2014-12-08 20:53:18 +0800

    baseSpringMVC

---
+++ baseSpringMVC/src/main/java/com/macrosoft/dao/IUserDao.java
@@ -0,0 +1,9 @@
+package com.macrosoft.dao;
+
+import com.macrosoft.model.User;
+
+public interface IUserDao {
+
+	void saveUser(User user);
+
+}
---
+++ baseSpringMVC/src/main/java/com/macrosoft/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.macrosoft.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.macrosoft.core.orm.hibernate.HibernateDao;
+import com.macrosoft.model.User;
+
+@Repository  
+public class UserDao extends HibernateDao<User, Integer>  implements IUserDao{
+
+	@Override
+	public void saveUser(User user) {
+		this.save(user);
+	}
+}
